0b01656d4b48697c3cfaf28659f3a98d79fa660a
znd <zeynepnurdoganer@gmail.com>
2025-11-24 09:07:51 +0300

    v0.0.1
    Modified version for axienet. QPLL reset is added.

--- drivers/net/ethernet/xilinx/xilinx_axienet_main.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_main.c
@@ -95,6 +95,16 @@ EXPORT_SYMBOL(mrmac_pll_reg);
 int mrmac_pll_rst;
 EXPORT_SYMBOL(mrmac_pll_rst);
 
+struct gpio_desc *rst_gpio;
+static void xilinx_qpll_reset(struct gpio_desc *chan)
+{
+	/* reset ip */
+	gpiod_set_value(chan, 0);
+	mdelay(100);
+	gpiod_set_value(chan, 1);
+	mdelay(100);
+};
+
 /* Option table for setting up Axi Ethernet hardware options */
 static struct axienet_option axienet_options[] = {
 	/* Turn on jumbo packet support for both Rx and Tx */
@@ -3426,6 +3436,20 @@ static int axienet_probe(struct platform_device *pdev)
 	u8 mac_addr[ETH_ALEN];
 	struct resource *ethres;
 	u32 value;
+
+rst_gpio = devm_gpiod_get(&pdev->dev, "reset",GPIOD_OUT_HIGH);
+
+if (IS_ERR(rst_gpio)) 
+{
+	err = PTR_ERR(rst_gpio);
+	if (err == -EPROBE_DEFER)
+		dev_info(&pdev->dev,"Probe deferred due to GPIO reset defer\n");
+        else
+		dev_err(&pdev->dev,"Unable to locate reset property in dt\n");
+}
+
+xilinx_qpll_reset(rst_gpio);
+
 #ifdef CONFIG_XILINX_AXI_EMAC_HWTSTAMP
 	struct resource txtsres, rxtsres;
 #endif
